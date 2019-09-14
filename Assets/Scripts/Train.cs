48b80531462452877021b0b8e34c6e6de8dab977
Markiewicz <emil.markiewicz@intel.com>
2019-09-14 13:59:59 +0200

    scrips update

---
+++ Assets/Scripts/Train.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Thomas thomas;
+    List<Wagon> wagons = new List<Wagon>();
+    void Start()
+    {
+        
+    }
+
+    public GoToPosition(Vector3 p)
+    {
+        this.transform.position = p;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
