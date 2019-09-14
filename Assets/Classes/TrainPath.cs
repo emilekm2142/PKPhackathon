255d853b664000fcb8615730fb1056cbbed3ddf0
Markiewicz <emil.markiewicz@intel.com>
2019-09-14 16:34:51 +0200

    xdx

--- Assets/Classes/TrainPath.cs
+++ Assets/Classes/TrainPath.cs
@@ -1,7 +1,11 @@
-﻿namespace Classes
-{
+﻿
+    using System.Collections.Generic;
+    using UnityEngine;
+[System.Serializable]
     public class TrainPath
     {
-        
+        public string name;
+        public Vector3 start;
+        public Vector3 end;
+        public Vector3 handle;
     }
-}
